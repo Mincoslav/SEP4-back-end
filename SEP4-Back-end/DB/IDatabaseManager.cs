a1d43797cc74efb03041d244434a4dd0d25ee6fe
EloLuigi <luis.freire@live.com.pt>
2020-04-29 13:46:41 +0200

    DatabaseManager update

---
+++ SEP4-Back-end/DB/IDatabaseManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SEP4_Back_end.Model;
+
+namespace SEP4_Back_end.DB
+{
+    public interface IDatabaseManager
+    {
+        void persistCO2(string co2,string roomName);
+        void persistHumdity(string humidity,string roomName);
+        void persistTemperature(string temperature,string roomName);
+        void persistServo(string servo,string roomName);
+        string getCO2(DateTime dateTime);
+        string getHumidity(DateTime dateTime);
+        string getTemperature(DateTime dateTime);
+        string getServo(DateTime dateTime);
+        string getCO2List(string roomName);
+        string getHumidityList(string roomName);
+        string getTemperatureList(string roomName);
+        string getServoList(string roomName);
+
+        Room getRoomByName(string name);
+    }
+}
